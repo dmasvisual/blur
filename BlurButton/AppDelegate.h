8649540fdff3efd2e95ea49966024a838682c15b
Pau <pau@MacBook-MacBook-Pro-de-Pau.local>
2015-09-02 13:02:39 +0200

    Initial Commit

---
+++ BlurButton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlurButton
+//
+//  Created by Pau on 2/9/15.
+//  Copyright (c) 2015 visual. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlurButton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlurButton
+//
+//  Created by Pau on 2/9/15.
+//  Copyright (c) 2015 visual. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
